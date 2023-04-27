c7be3f982c926aaf529a076be7e72fd3c1610623
billymaynard <billymaynardire@gmail.com>
2023-04-27 20:38:20 +0200

    Made changes to the GUI and implemented the necessary classes to the controller to generate answer correctly. Surprisingly no further modifications are needed, program is working correctly at this time...

--- src/main/java/com/zapping/zappingtochannel/ZappingController.java
+++ src/main/java/com/zapping/zappingtochannel/ZappingController.java
@@ -11,6 +11,8 @@ import java.util.ResourceBundle;
 
 public class ZappingController implements Initializable {
     @FXML
+    private Label Awnser;
+    @FXML
     private Spinner CurrentSpinner;
     @FXML
     private Spinner DesiredSpinner;
@@ -22,5 +24,13 @@ public class ZappingController implements Initializable {
         SpinnerValueFactory<Integer> DesiredSpinnerFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,99);
         this.CurrentSpinner.setValueFactory(CurrentSpinnerFactory);
         this.DesiredSpinner.setValueFactory(DesiredSpinnerFactory);
+    }
+    @FXML
+    public void calculate(){
+        Channels setofchannels = new Channels(99);
+        Direction direction = setofchannels.closestto((Integer) CurrentSpinner.getValue(), (Integer) DesiredSpinner.getValue());
+        Awnser.setText(direction.getDirection()+" by "+direction.clicks+" to reach your channel");
+        SpinnerValueFactory<Integer> CurrentSpinnerFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,99, (Integer) DesiredSpinner.getValue());
+        CurrentSpinner.setValueFactory(CurrentSpinnerFactory);
     }
 }
