b2381d24bbb51ee937b29d6ca3edd1ab5121b1ff
billymaynard <billymaynardire@gmail.com>
2023-04-27 20:52:22 +0200

    Added second test, no modifications to the main code needed.

--- src/main/Tests/com/zapping/zappingtochannel/ChannelsTest.java
+++ src/main/Tests/com/zapping/zappingtochannel/ChannelsTest.java
@@ -9,7 +9,7 @@ class ChannelsTest {
 
     @Test
     @DisplayName("Canal Destino 0")
-    void canaldestino0() {
+    void test1() {
         //Arrange
         Channels setofChannels = new Channels(99);
         Direction newdirection;
@@ -19,5 +19,18 @@ class ChannelsTest {
 
         //Assert
         assertEquals(0,newdirection.getClicks());
+    }
+    @Test
+    @DisplayName("Canal Destino 2, Canal Actual 1")
+    void test2() {
+        //Arrange
+        Channels setofChannels = new Channels(99);
+        Direction newdirection;
+
+        //Act
+        newdirection = setofChannels.closestto(1,2);
+
+        //Assert
+        assertEquals(1,newdirection.getClicks());
     }
 }
