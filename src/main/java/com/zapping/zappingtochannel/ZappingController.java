8b4e7c1384ebb3975b2cfcd3f29c87f3a51562a5
billymaynard <billymaynardire@gmail.com>
2023-04-27 17:50:41 +0200

    Finished GUI for the Zapping Program

---
+++ src/main/java/com/zapping/zappingtochannel/ZappingController.java
@@ -0,0 +1,26 @@
+package com.zapping.zappingtochannel;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ZappingController implements Initializable {
+    @FXML
+    private Spinner CurrentSpinner;
+    @FXML
+    private Spinner DesiredSpinner;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        SpinnerValueFactory<Integer> CurrentSpinnerFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,99);
+        SpinnerValueFactory<Integer> DesiredSpinnerFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,99);
+        this.CurrentSpinner.setValueFactory(CurrentSpinnerFactory);
+        this.DesiredSpinner.setValueFactory(DesiredSpinnerFactory);
+    }
+}
--- src/main/java/com/zapping/zappingtochannel/ZappingToChannelApp.java
+++ src/main/java/com/zapping/zappingtochannel/ZappingToChannelApp.java
@@ -1,18 +1,20 @@
 package com.zapping.zappingtochannel;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Spinner;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ZappingToChannelApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ZappingToChannelApp.class.getResource("channel_picker.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Channel Picker!");
         stage.setScene(scene);
         stage.show();
     }
